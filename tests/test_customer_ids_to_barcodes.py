75699deec664f7d14aa5936f72133890ad762823
Nikolai Linnansalo <nikolai@vainu.io>
2019-12-03 11:56:53 +0100

    added test that ensures that a csv file is created

--- tests/test_customer_ids_to_barcodes.py
+++ tests/test_customer_ids_to_barcodes.py
@@ -1,7 +1,15 @@
+import os
+from time import time
 from customer_ids_to_barcodes import make_customer_ids_to_barcodes_csv_with_pandas
 
 
 def test_customer_ids_to_barcordes():
 
-    make_customer_ids_to_barcodes_csv_with_pandas()
+    test_filename = f"test_file_{time()}.csv"
+    test_filepath = os.path.join("tests", test_filename)
+
+    make_customer_ids_to_barcodes_csv_with_pandas(test_filepath)
+
+    if os.path.exists(test_filepath):
+        os.remove(test_filepath)
     assert True
